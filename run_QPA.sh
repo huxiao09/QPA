7e9e49e4291b0af4aaded913a02c15dcd13cce1e
Hu Xiao <hfhuxiao@163.com>
2024-09-25 00:03:32 +0800

    max_feedback=100 in cheetah_run

--- run_QPA.sh
+++ run_QPA.sh
@@ -7,7 +7,7 @@ done
 
 # cheetah_run:
 for seed in 1 2 3 4 5; do
-python train_QPA.py env=cheetah_run agent.params.actor_lr=0.0005 agent.params.critic_lr=0.0005 gradient_update=1 activation=tanh num_unsup_steps=9000 num_train_steps=1000000 num_interact=20000 max_feedback=250 reward_batch=10 reward_update=2000 teacher_beta=-1 teacher_gamma=1 teacher_eps_mistake=0 teacher_eps_skip=0 teacher_eps_equal=0 segment=50 max_reward_buffer_size=10 data_aug_ratio=20 ensemble_size=1 explore=false her_ratio=0.5 wandb=true device='cuda' seed=$seed
+python train_QPA.py env=cheetah_run agent.params.actor_lr=0.0005 agent.params.critic_lr=0.0005 gradient_update=1 activation=tanh num_unsup_steps=9000 num_train_steps=1000000 num_interact=20000 max_feedback=100 reward_batch=10 reward_update=2000 teacher_beta=-1 teacher_gamma=1 teacher_eps_mistake=0 teacher_eps_skip=0 teacher_eps_equal=0 segment=50 max_reward_buffer_size=10 data_aug_ratio=20 ensemble_size=1 explore=false her_ratio=0.5 wandb=true device='cuda' seed=$seed
 done
 
 # walker_run:
